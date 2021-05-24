d725bb8d80aa71067c5be60bae8fc11011fcf472
Ruslan <ruslan@gmail.com>
2021-05-24 18:38:12 +0300

    block1_media

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.hamburger'),
+/* const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
         
@@ -16,18 +16,36 @@ const percents = document.querySelectorAll('.skills__progress-percent'),
 
 percents.forEach( (item,i) => {
     lines[i].style.width = item.innerHTML;
-});
+}); */
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1600) {
-        $('.pageup').fadeIn();
-    } else {
-        $('.pageup').fadeOut();
-    } 
-});
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    dotsClass: 'slick-dots',
+                    arrows: false
+                }
+            }
+        ]
+    });
 
-$("a[href=#up]").click(function(){
-    const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    return false;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        } 
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
